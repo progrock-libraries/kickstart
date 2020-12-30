021d12fbcd5416e0d03dbaab5ff6619a60a7e831
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2020-12-30 16:51:17 +0100

    Comments cleanup

--- source/library/core/text_conversion.hpp
+++ source/library/core/text_conversion.hpp
@@ -2,11 +2,6 @@
 #pragma once
 #include "../assertion-headers/assert-reasonable-compiler.hpp"
 
-// kickstart.core.hpp - minimal convenience functionality for C++ learners.
-// The “core” is because the (rather large) iostreams headers are not included.
-//
-// Requires C++17 or later.
-
 // Copyright (c) 2020 Alf P. Steinbach. MIT license, with license text:
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -15,10 +10,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
