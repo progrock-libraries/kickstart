c39adb24cf664dfbbac30dd2f7a97f6bed500776
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2021-01-18 13:31:36 +0100

    Update personalized-greeting.solid.cpp

--- source/examples/utf8-io/input/personalized-greeting.solid.cpp
+++ source/examples/utf8-io/input/personalized-greeting.solid.cpp
@@ -1,11 +1,13 @@
 ﻿// Source encoding: utf-8  --  π is (or should be) a lowercase greek pi.
 #include <kickstart/all.hpp>
-KS_ASSERT_UTF8_LITERALS();  using namespace kickstart::all;
+KS_ASSERT_UTF8_LITERALS();
+using namespace kickstart::all;
 
 void cpp_main()
 {
-    const string answer = ascii::trimmed( input( "Hi, what’s your name? ") );
-    const string_view username = (answer.empty()? "“Nomen Nescio”"sv : answer);
+    const string        answer      = ascii::trimmed( input( "Hi, what’s your name? ") );
+    const string_view   username    = (answer.empty()? "“Nomen Nescio”"sv : answer);
+
     output( ""s << "Greetings, " << username << "! Happy to meet you!\n" );
 }
 
