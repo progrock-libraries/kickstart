cc227f373842c61ec8026427d559351b81eaa8eb
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2021-01-29 09:44:27 +0100

    Doc work

---
+++ documentation/tutorial/examples/personalized-kickstart-greeting.cpp
@@ -0,0 +1,8 @@
+﻿#include <kickstart/all.hpp>
+using namespace kickstart::all;
+
+auto main() -> int
+{
+    const string name = input( "Hi, what’s your name? " );
+    out << "Welcome to the Kickstart experience, " << name << "!" << endl;
+}
