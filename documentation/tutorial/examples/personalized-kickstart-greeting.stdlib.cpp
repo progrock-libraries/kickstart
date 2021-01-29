562c6b287c6c9acd37d4800e4dd170ebdab2117b
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2021-01-29 12:38:36 +0100

    Doc work

---
+++ documentation/tutorial/examples/personalized-kickstart-greeting.stdlib.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+auto fail() -> bool { throw "Gah!"; }
+
+auto main() -> int
+{
+    string name;
+    cout << "Hi, what’s your name? ";
+    getline( cin, name ) or fail();
+    cout << "Welcome to the C++ standard library experience, " << name << "!" << endl;
+}
