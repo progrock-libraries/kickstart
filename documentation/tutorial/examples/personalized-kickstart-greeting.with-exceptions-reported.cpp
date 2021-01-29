111a2915e5fb622a117879ee1623dc69a5572001
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2021-01-29 14:06:44 +0100

    Doc work

---
+++ documentation/tutorial/examples/personalized-kickstart-greeting.with-exceptions-reported.cpp
@@ -0,0 +1,10 @@
+﻿#include <kickstart/all.hpp>
+using namespace kickstart::all;
+
+void cppmain()
+{
+    const string name = input( "Hi, what’s your name? " );
+    out << "Welcome to the Kickstart experience, " << name << "!" << endl;
+}
+
+auto main() -> int { return with_exceptions_displayed( cppmain ); }
