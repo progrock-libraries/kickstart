d13b291ca7f146d295d4690c63a46642a3171cbe
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2021-01-31 05:16:01 +0100

    Doc work

---
+++ documentation/tutorial/examples/floating-point-formatting.cpp
@@ -0,0 +1,13 @@
+#include <kickstart/all.hpp>
+using namespace kickstart::all;
+
+auto main() -> int
+{
+    double x = math::pi;
+    for( int i = 1; i <= 5; ++i, x *= 1000 ) {
+        out << at_right_in( 20, str( x ) )
+            << at_right_in( 20, to_fixed( x, 4 ) )
+            << at_right_in( 20, to_scientific( x, 4 ) )
+            << endl;
+    }
+}
