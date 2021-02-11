5d18645eef918bde8486f7204476bbb4e9c9cdc0
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2021-02-11 22:40:45 +0100

    -

--- source/library/core/stdlib-extensions/c-files.hpp
+++ source/library/core/stdlib-extensions/c-files.hpp
@@ -22,6 +22,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include 
+//#include <kickstart/>
+#include <stdio.h>
+
 namespace kickstart::stdlib_extensions::files::_definitions {
 }  // namespace kickstart::stdlib_extensions::files::_definitions
