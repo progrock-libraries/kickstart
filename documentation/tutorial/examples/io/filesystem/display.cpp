195bb7d6fd2fed83db5808b4016cd9c2bd742ab4
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2021-02-25 17:24:37 +0100

    Doc work.

---
+++ documentation/tutorial/examples/io/filesystem/display.cpp
@@ -0,0 +1,19 @@
+﻿#include <kickstart/all.hpp>
+using namespace kickstart::all;
+
+void cppmain()
+{
+    const string_view filename = process::the_commandline().args().at( 0 );
+    auto reader = Text_reader( filename );
+
+    while( const optional<string> line = reader.input_or_eof() ) {
+        out << line << "\n";
+    }
+
+    hopefully( reader.has_passed_eof() )
+        or fail_app( "Reading from “"s << filename << "” failed." );
+}
+
+auto main( int n_cmd_parts, char** cmd_parts )
+    -> int
+{ return with_exceptions_displayed( cppmain, n_cmd_parts, cmd_parts ); }
