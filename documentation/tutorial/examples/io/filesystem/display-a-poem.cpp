90e38e2881aba53e3b9288b9b274376b64ea0cf5
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2021-02-26 01:04:01 +0100

    Doc work

--- documentation/tutorial/examples/io/filesystem/display-a-poem.cpp
+++ documentation/tutorial/examples/io/filesystem/display-a-poem.cpp
@@ -3,15 +3,9 @@ using namespace kickstart::all;
 
 void cppmain()
 {
-    const auto&         filename    = "poem.txt";
-    const fsx::Path     path        = fsx::path_of_exe_relative( filename );
-    
+    const fsx::Path path = fsx::path_of_exe_relative( "poem.txt" );
     auto reader = Text_reader( path );
-    while( const optional<string> line = reader.input_or_eof() ) {
-        out << line << "\n";
-    }
-    hopefully( reader.has_passed_eof() )
-        or fail_app( "Reading from “"s << filename << "” failed." );
+    while( const auto line = reader.input_or_eof() ) { out << line << "\n"; }
 }
 
 auto main() -> int { return with_exceptions_displayed( cppmain ); }
