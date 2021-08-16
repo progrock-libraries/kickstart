58430dafdaabf9eb58dd8ee14b445b777d4cfaa0
alf-p-steinbach <alf.p.steinbach@gmail.com>
2021-08-16 06:42:35 +0200

    -

--- source/examples/motivational/name-and-age.kickstart.cpp
+++ source/examples/motivational/name-and-age.kickstart.cpp
@@ -1,7 +1,7 @@
-﻿#include <kickstart/all.hpp>
+﻿#include <kickstart+main.hpp>
 using namespace kickstart::all;
 
-auto main() -> int
+void cpp_main()
 {
     const string    name    = input( "Enter your name, please: " );
     const int       age     = to_<int>( input( "What’s your age, in years? " ) );
